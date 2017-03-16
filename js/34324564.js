245ac0b7ef20917cf409e3e178a94a4a91316eef
jose-douglas-ramirez <jose.douglas.ramirez@outlook.com>
2017-03-16 12:31:03 -0500

    Update 34324564.js

--- js/34324564.js
+++ js/34324564.js
@@ -1,8 +1,11 @@
 
-try{
-   debugger
+try{   
    angular.element(document).ready(function () {
-        debugger
+        setTimeout(function(){  
+           debugger
+           var login = $("#pcont > div.cl-mcont.ng-scope > div.row.login-row.ng-scope > div > div.block-flat.login-bf > div.row > div > div > form > div.row > div");          
+           login.hide();
+        }, 3000);
     });
 }
 catch(err){
